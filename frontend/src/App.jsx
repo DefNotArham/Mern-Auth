58948329bf4fa7fe88849e4872ee570a1b7a7c1a
DefNotArham <arhamkabir231@gmail.com>
2026-03-24 14:07:59 -0400

    protected routes and redirect routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,67 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup.jsx";
 import Login from "./pages/Login.jsx";
 import VerifyEmail from "./pages/VerifyEmail.jsx";
+import { useAuthStore } from "./store/authStore.js";
+import { useEffect } from "react";
+import HomePage from "./pages/HomePage.jsx";
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (!user.isVerified) {
+    return <Navigate to="/verify-email" replace />;
+  }
+  return children;
+};
+
+const RedirectAuthenticatedUser = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+  if (isAuthenticated && user.isVerified) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
 
 function App() {
+  const { checkAuth, isAuthenticated, user } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log("isAuthenticated", isAuthenticated);
+  console.log("isAuthenticated", user);
   return (
     <Routes>
-      <Route path="/" element={<h1>HomePage</h1>} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <RedirectAuthenticatedUser>
+            <Signup />
+          </RedirectAuthenticatedUser>
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <RedirectAuthenticatedUser>
+            <Login />
+          </RedirectAuthenticatedUser>
+        }
+      />
       <Route path="/verify-email" element={<VerifyEmail />} />
     </Routes>
   );
